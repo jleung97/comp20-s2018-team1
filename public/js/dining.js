a6fc0d3063a82bb49f5a7c4940792f2406edff32
Jennifer Leung <jenniferleung9@gmail.com>
2018-04-27 19:46:11 -0400

    make menu items collapsible, connect js files

--- public/js/dining.js
+++ public/js/dining.js
@@ -1,34 +1,37 @@
-var dining_container = document.getElementById('dining_info');
-var request = new XMLHttpRequest();
-var dateObj = new Date();
-var month = dateObj.getMonth() + 1; //months from 1-12
-var day = dateObj.getDate();
-var year = dateObj.getFullYear();
-var url = 'https://tuftsdiningdata.herokuapp.com/rmenus/dewick/' + day + '/' + month + '/' + year;
-var menu = ''
+function get_menu(dining_hall) {
+	var request = new XMLHttpRequest();
+	var dateObj = new Date();
+	var month = dateObj.getMonth() + 1; //months from 1-12
+	var day = dateObj.getDate();
+	var year = dateObj.getFullYear();
+	var url = 'https://tuftsdiningdata.herokuapp.com/menus/' + dining_hall + '/' + day + '/' + month + '/' + year;
 
-request.open("GET", url, true);
-request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-request.onreadystatechange = function() {
-	if (request.readyState == 4 && request.status == 200) {
-		response = request.responseText;
-		response = JSON.parse(response)['data'];
-		var meals = ['Breakfast', 'Lunch', 'Dinner'];
-		for (i = 0; i < meals.length; i++) {
-			meal = meals[i];
-			menu += '<p class="meal">' + meal.toUpperCase() + '</p>';
-			for (category in response[meal]) {
-				menu += '<span class="category">' + toTitleCase(category) + '</span><ul>';
-				for (food in response[meal][category]) {
-					menu += '<li class="food">' + response[meal][category][food] + '</li>';
-				}
-				menu += '</ul>';
-			} 
+	request.open("GET", url, true);
+	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	request.onreadystatechange = function() {
+		if (request.readyState == 4 && request.status == 200) {
+			response = request.responseText;
+			response = JSON.parse(response)['data'];
+
+			var meals = ['Breakfast', 'Lunch', 'Dinner'];
+			for (i = 0; i < meals.length; i++) {
+				meal = meals[i];
+				section = document.getElementById(meal + '_body');
+				var menu = '';
+				for (category in response[meal]) {
+					menu += '<span class="category">' + toTitleCase(category) + '</span><ul>';
+					for (food in response[meal][category]) {
+						menu += '<li>' + response[meal][category][food] + '</li>';
+					}
+					menu += '</ul>';
+				} 
+				section.innerHTML = menu;
+			}
 		}
-		dining_container.innerHTML = menu;
 	}
+	request.send();
+
 }
-request.send();
 
 //https://stackoverflow.com/questions/4878756/how-to-capitalize-first-letter-of-each-word-like-a-2-word-city
 function toTitleCase(str)
